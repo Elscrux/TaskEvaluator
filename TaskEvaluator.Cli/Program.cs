0b1e4126ddc32307a0497313beba7f3c8f1633fb
Elscrux <nickposer2102@gmail.com>
2024-01-26 15:21:19 +0100

    Include user secrets in Cli

--- TaskEvaluator.Cli/Program.cs
+++ TaskEvaluator.Cli/Program.cs
@@ -1,13 +1,16 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TaskEvaluator.Cli;
 using TaskEvaluator.Modules;
 using TaskEvaluator.Runtime.Implementation.CSharp;
+using TaskEvaluator.Tasks;
 
 var builder = Host.CreateApplicationBuilder();
 builder.Services.AddLogging();
 builder.Services.AddHttpClient();
 
+builder.Configuration.AddUserSecrets<TaskRunner>();
 builder.Services.AddTaskEvaluator();
 builder.Services.AddCSharp();
 
