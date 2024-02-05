9e586017e0ab8cb1319a9108770fb7aa05700257
Elscrux <nickposer2102@gmail.com>
2024-02-05 22:39:33 +0100

    Update Code
    - Now JSON deserializable
    - Added root file name

--- TaskEvaluator/Tasks/Code.cs
+++ TaskEvaluator/Tasks/Code.cs
@@ -1,7 +1,19 @@
 ﻿using TaskEvaluator.Language;
 namespace TaskEvaluator.Tasks;
 
-public sealed record Code(Guid Guid, string Body, ProgrammingLanguage Language) {
-    public Code(string body, ProgrammingLanguage language) : this(Guid.NewGuid(), body, language) {}
-    public string Body { get; init; } = Body.Replace("\r\n", "\n");
+public sealed record Code {
+    public Code(string Body, ProgrammingLanguage Language) {
+        this.Language = Language;
+        this.Body = Body.Replace("\r\n", "\n");
+    }
+
+    public string Body { get; init; }
+    public Guid Guid { get; init; } = Guid.NewGuid();
+    public ProgrammingLanguage Language { get; init; }
+
+
+    public string RootFileName => Language switch {
+        ProgrammingLanguage.CSharp => "Program.cs",
+        _ => throw new NotSupportedException($"Language {Language} is not supported.")
+    };
 }
