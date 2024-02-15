8171330ce6e5f52665de399b7b3d7038a411189a
Elscrux <nickposer2102@gmail.com>
2024-02-15 22:42:31 +0100

    Fix not using unit tests for unit test call

--- TaskEvaluator/Evaluator/UnitTest/UnitTestEvaluator.cs
+++ TaskEvaluator/Evaluator/UnitTest/UnitTestEvaluator.cs
@@ -4,7 +4,9 @@ namespace TaskEvaluator.Evaluator.UnitTest;
 
 public sealed class UnitTestEvaluator(IRuntime runtime) : IRuntimeEvaluator {
     public async Task<IEvaluationResult> Evaluate(Code code, EvaluationModel evaluationModel, CancellationToken token = default) {
-        var runtimeResult = await runtime.UnitTest(code, token);
+        if (evaluationModel.UnitTests is null) return new UnitTestEvaluationResult(code.Guid, false, "No unit tests provided.");
+
+        var runtimeResult = await runtime.UnitTest(evaluationModel.UnitTests, token);
 
         return new UnitTestEvaluationResult(code.Guid, runtimeResult);
     }
