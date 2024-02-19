387f2ce3c3e82716aec915eeeca93ae94ce19a88
Elscrux <nickposer2102@gmail.com>
2024-02-19 15:44:59 +0100

    Serialize derived types

--- TaskEvaluator/Evaluator/IEvaluationResult.cs
+++ TaskEvaluator/Evaluator/IEvaluationResult.cs
@@ -1,5 +1,10 @@
-﻿namespace TaskEvaluator.Evaluator;
+﻿using System.Text.Json.Serialization;
+using TaskEvaluator.Evaluator.StaticCodeAnalysis;
+using TaskEvaluator.Evaluator.UnitTest;
+namespace TaskEvaluator.Evaluator;
 
+[JsonDerivedType(typeof(StaticCodeEvaluationResult))]
+[JsonDerivedType(typeof(UnitTestEvaluationResult))]
 public interface IEvaluationResult {
     Guid TaskId { get; }
     bool Success { get; }
