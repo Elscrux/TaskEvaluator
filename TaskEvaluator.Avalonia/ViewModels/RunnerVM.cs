f4cf94f3bc5b87a7e54f646e638a9a48d2a5e685
Elscrux <nickposer2102@gmail.com>
2024-02-26 00:06:33 +0100

    Add run all button

--- TaskEvaluator.Avalonia/ViewModels/RunnerVM.cs
+++ TaskEvaluator.Avalonia/ViewModels/RunnerVM.cs
@@ -1,21 +1,34 @@
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
 using Avalonia.Threading;
 using DynamicData.Binding;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using TaskEvaluator.Sink.PostgreSQL;
 using TaskEvaluator.Tasks;
 namespace TaskEvaluator.Avalonia.ViewModels;
 
 public sealed class RunnerVM : ViewModel, IRunnerVM {
     public IObservableCollection<ITaskVM> Tasks { get; }
+    public ReactiveCommand<Unit, Task> RunAll { get; }
     public ReactiveCommand<Unit, Unit> LoadFromDatabase { get; }
+
+    [Reactive] public int Parallelism { get; set; } = 5;
 
     public RunnerVM(
         PostgresFinalResultSink postgresFinalResultSink,
         TaskEvaluatorVMFactory taskEvaluatorVMFactory,
         ITaskLoader taskLoader) {
         Tasks = new ObservableCollectionExtended<ITaskVM>(taskLoader.Load().Select(taskEvaluatorVMFactory.TaskVM));
+
+        RunAll = ReactiveCommand.CreateRunInBackground(async () => {
+            await Tasks.AwaitAll(async vm => {
+                await await vm.RunAll.Execute();
+            }, Parallelism);
+        });
+
         LoadFromDatabase = ReactiveCommand.CreateRunInBackground(() => {
             var taskResults = postgresFinalResultSink.Retrieve()
                 .GroupBy(x => x.CodeGenerationResult.TaskId)
@@ -36,6 +49,9 @@ public sealed class RunnerVM : ViewModel, IRunnerVM {
 
 public sealed class DesignRunnerVM : ViewModel, IRunnerVM {
     public IObservableCollection<ITaskVM> Tasks { get; }
+    public int Parallelism { get; set; }
+
+    public ReactiveCommand<Unit, Task> RunAll { get; } = ReactiveCommand.Create(() => Task.CompletedTask);
     public ReactiveCommand<Unit, Unit> LoadFromDatabase { get; } = ReactiveCommand.Create(() => {});
 
     public DesignRunnerVM() {
@@ -48,5 +64,8 @@ public sealed class DesignRunnerVM : ViewModel, IRunnerVM {
 
 public interface IRunnerVM {
     IObservableCollection<ITaskVM> Tasks { get; }
+    int Parallelism { get; set; }
+
+    public ReactiveCommand<Unit, Task> RunAll { get; }
     ReactiveCommand<Unit, Unit> LoadFromDatabase { get; }
 }
