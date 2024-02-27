f566181b946c96413c379cc6f8599326b78c97c4
Elscrux <nickposer2102@gmail.com>
2024-02-27 14:39:50 +0100

    Add test

--- TaskSet/CSharp/DecodeCyclic/UnitTests.cs
+++ TaskSet/CSharp/DecodeCyclic/UnitTests.cs
@@ -2,5 +2,14 @@ using Xunit;
 namespace Task;
 
 public class Test_DecodeCyclic {
-
+    [Fact]
+    public void Test_0() {
+        const string letters = "abcdefghijklmnopqrstuvwxyz";
+        foreach (var i in Enumerable.Range(0, 100)) {
+            var str = string.Join(string.Empty, Enumerable.Range(0, 10).Select(_ => letters[new Random().Next(letters.Length)]));
+            var encodedStr = TaskClass.EncodeCyclic(str);
+            var decodedStr = TaskClass.DecodeCyclic(encodedStr);
+            Assert.Equal(str, decodedStr);
+        }
+    }
 }
