dda94f180c82fed5959cac5203698cb88a38a727
Elscrux <nickposer2102@gmail.com>
2024-03-10 23:31:16 +0100

    Add missing tests

--- TaskSet/CSharp/DecodeShift/UnitTests.cs
+++ TaskSet/CSharp/DecodeShift/UnitTests.cs
@@ -2,5 +2,14 @@ using Xunit;
 namespace Task;
 
 public class Test_DecodeShift {
-
+    [Fact]
+    public void Test_0() {
+        const string letters = "abcdefghijklmnopqrstuvwxyz";
+        foreach (var i in Enumerable.Range(0, 100)) {
+            var str = string.Join(string.Empty, Enumerable.Range(0, 10).Select(_ => letters[new Random().Next(letters.Length)]));
+            var encodedStr = TaskClass.EncodeShift(str);
+            var decodedStr = TaskClass.DecodeShift(encodedStr);
+            Assert.Equal(str, decodedStr);
+        }
+    }
 }
--- TaskSet/CSharp/FindZero/UnitTests.cs
+++ TaskSet/CSharp/FindZero/UnitTests.cs
@@ -2,5 +2,22 @@ using Xunit;
 namespace Task;
 
 public class Test_FindZero {
+    [Fact]
+    public void Test_0() {
+        var random = new Random(42);
+        for (var i = 0; i < 100; i++) {
+            var ncoeff = 2 * random.Next(1, 4);
+            List<double> coeffs = [];
+            for (var j = 0; j < ncoeff; j++) {
+                var coeff = random.Next(-10, 10);
+                if (coeff == 0) coeff = 1;
 
+                coeffs.Add(coeff);
+            }
+
+            var solution = TaskClass.FindZero(coeffs);
+            var zero = TaskClass.Poly(coeffs, solution);
+            Assert.True(Math.Abs(zero)  < 1e-4, $"Expected 0 but got {zero} for coefficients {string.Join(", ", coeffs)}");
+        }
+    }
 }
